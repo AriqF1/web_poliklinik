ec6662a328b75729da7d95c6262ed48f7d6d5e34
Ariq Farhan <ariqalthaf445@gmail.com>
2025-04-10 21:02:28 +0700

    Update : Prettier routes

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,44 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DokterController;
 use App\Http\Controllers\MemeriksaController;
 use App\Http\Controllers\ObatController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PasienController;
+use App\Http\Controllers\PeriksaController;
 
 Route::get('/', function () {
     return view('layout.app');
 });
 
-Route::get('/dokter/dashboard', [DokterController::class, 'index'])->name('dokter.index');
+// Dashboard Pasien
+Route::prefix('pasien')->name('pasien.')->group(function () {
 
-Route::get('/dokter/memeriksa', [MemeriksaController::class, 'index'])->name('dokter.memeriksa');
-
-
-Route::get('/dokter/obat', [ObatController::class, 'index'])->name('dokter.obat');
-Route::post('/dokter/obat', [ObatController::class, 'store'])->name('dokter.obat.store');
-Route::get('/dokter/obat/create', [ObatController::class, 'create'])->name('dokter.obat.create');
-Route::get('/dokter/obat/{id}/edit', [ObatController::class, 'edit'])->name('dokter.obat.edit');
-Route::put('/dokter/obat/{id}', [ObatController::class, 'update'])->name('dokter.obat.update');
-Route::delete('/dokter/obat/{id}', [ObatController::class, 'destroy'])->name('dokter.obat.destroy');
+    // Dashboard Pasien
+    Route::get('/dashboard', [PasienController::class, 'index'])->name('index');
 
 
-Route::get('/pasien/dashboard', function () {
-    return view('pasien.index');
+    Route::get('/periksa', [PeriksaController::class, 'index'])->name('periksa');
+    Route::get('/periksa/create', [PeriksaController::class, 'create'])->name('periksa.create');
+    Route::post('/periksa', [PeriksaController::class, 'store'])->name('periksa.store');
 });
 
+// Group Dokter
+Route::prefix('dokter')->name('dokter.')->group(function () {
 
+    // Dashboard Dokter
+    Route::get('/dashboard', [DokterController::class, 'index'])->name('index');
+
+    // Memeriksa (Index = Form Create + List)
+    Route::get('/memeriksa', [MemeriksaController::class, 'index'])->name('memeriksa');
+    Route::get('/memeriksa/create', [MemeriksaController::class, 'create'])->name('memeriksa.create');
+    Route::post('/memeriksa', [MemeriksaController::class, 'store'])->name('memeriksa.store');
+
+    // Obat
+    Route::get('/obat', [ObatController::class, 'index'])->name('obat');
+    Route::get('/obat/create', [ObatController::class, 'create'])->name('obat.create');
+    Route::post('/obat', [ObatController::class, 'store'])->name('obat.store');
+    Route::get('/obat/{id}/edit', [ObatController::class, 'edit'])->name('obat.edit');
+    Route::put('/obat/{id}', [ObatController::class, 'update'])->name('obat.update');
+    Route::delete('/obat/{id}', [ObatController::class, 'destroy'])->name('obat.destroy');
+});
