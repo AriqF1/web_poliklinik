cd83bbdf6dbc7b5c13369535414c97a5af15fc27
Farhan Ariq <122168324+AriqF1@users.noreply.github.com>
2025-04-16 20:48:13 +0700

    Add : New template app

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,183 +1,139 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title> @yield('title') </title>
-
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{asset('lte/plugins/fontawesome-free/css/all.min.css')}}">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet"
-        href="{{asset('lte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="{{asset('lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
-    <!-- JQVMap -->
-    <link rel="stylesheet" href="{{asset('lte/plugins/jqvmap/jqvmap.min.css')}}">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('lte/dist/css/adminlte.min.css')}}">
-    <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="{{asset('lte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
-    <!-- Daterange picker -->
-    <link rel="stylesheet" href="{{asset('lte/plugins/daterangepicker/daterangepicker.css')}}">
-    <!-- summernote -->
-    <link rel="stylesheet" href="{{asset('lte/plugins/summernote/summernote-bs4.min.css')}}">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style-create.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 
-<body class="hold-transition sidebar-mini layout-fixed">
-    <div class="wrapper">
-
-        <!-- Preloader -->
-        <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60"
-                width="60">
-        </div>
-
-        <!-- Navbar -->
-        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-            <!-- Left navbar links -->
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-                </li>
-                <li class="nav-item d-none d-sm-inline-block">
-                    <a href="index3.html" class="nav-link">Home</a>
-                </li>
-                <li class="nav-item d-none d-sm-inline-block">
-                    <a href="#" class="nav-link">Contact</a>
-                </li>
-            </ul>
-
-            <!-- Right navbar links -->
-            <ul class="navbar-nav ml-auto">
-                <!-- Navbar Search -->
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-                        <i class="fas fa-search"></i>
-                    </a>
-                    <div class="navbar-search-block">
-                        <form class="form-inline">
-                            <div class="input-group input-group-sm">
-                                <input class="form-control form-control-navbar" type="search" placeholder="Search"
-                                    aria-label="Search">
-                                <div class="input-group-append">
-                                    <button class="btn btn-navbar" type="submit">
-                                        <i class="fas fa-search"></i>
-                                    </button>
-                                    <button class="btn btn-navbar" type="button" data-widget="navbar-search">
-                                        <i class="fas fa-times"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </li>
-            </ul>
-        </nav>
-        <!-- /.navbar -->
-
-        <!-- Main Sidebar Container -->
-        <aside class="main-sidebar sidebar-dark-primary elevation-4">
-            <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
-                <img src="{{asset('lte/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo"
-                    class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">Dashboard @yield('dashboard')</span>
-            </a>
-
-            <!-- Sidebar -->
-            <div class="sidebar">
-                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                    <div class="image">
-                        <img src="{{asset('lte/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2"
-                            alt="User Image">
-                    </div>
-                    <div class="info">
-                        <a href="#" class="d-block">Alexander Pierce</a>
-                    </div>
-                </div>
-
-                <!-- SidebarSearch Form -->
-                <div class="form-inline">
-                    <div class="input-group" data-widget="sidebar-search">
-                        <input class="form-control form-control-sidebar" type="search" placeholder="Search"
-                            aria-label="Search">
-                        <div class="input-group-append">
-                            <button class="btn btn-sidebar">
-                                <i class="fas fa-search fa-fw"></i>
-                            </button>
+<body>
+    <div class="container">
+        <!-- Sidebar -->
+        <div class="sidebar">
+            @if(auth()->check())
+                @if(auth()->user()->role === 'pasien')
+                    <div class="sidebar-header">
+                        <div class="sidebar-brand">
+                            <i class="fas fa-heartbeat"></i>
+                            <span>SISMENKES</span>
                         </div>
                     </div>
-                </div>
+                    <div class="sidebar-menu">
+                        <a href="{{ Route('pasien.index') }}" class="menu-item active">
+                            <i class="fas fa-chart-line"></i>
+                            <span>Dashboard</span>
+                        </a>
+                        <a href="{{ Route('pasien.periksa') }}" class="menu-item">
+                            <i class="fas fa-stethoscope"></i>
+                            <span>Periksa</span>
+                        </a>
+                        <a href="#" class="menu-item"
+                            onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                            <i class="nav-icon fas fa-arrow-right-from-bracket"></i>
+                            <p>Logout</p>
+                        </a>
 
-                <!-- Sidebar Menu -->
-                <nav class="mt-2">
-                    @yield('sidebar')
-                </nav>
-            </div>
-        </aside>
+                        <!-- Hidden Logout Form -->
+                        <form id="logout-form" action="{{ route('pasien.logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                @elseif(auth()->user()->role === 'dokter')
+                    <div class="sidebar-header">
+                        <div class="sidebar-brand">
+                            <i class="fas fa-heartbeat"></i>
+                            <span>SISMENKES</span>
+                        </div>
+                    </div>
+                    <div class="sidebar-menu">
+                        <a href="{{ Route('dokter.index') }}" class="menu-item active">
+                            <i class="fas fa-chart-line"></i>
+                            <span>Dashboard</span>
+                        </a>
+                        <a href="{{ Route('dokter.memeriksa') }}" class="menu-item">
+                            <i class="fas fa-stethoscope"></i>
+                            <span>Memeriksa</span>
+                        </a>
+                        <a href="{{ Route('dokter.obat') }}" class="menu-item">
+                            <i class="fas fa-pills"></i>
+                            <span>Obat</span>
+                        </a>
+                        <a href="#" class="menu-item"
+                            onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                            <i class="nav-icon fas fa-arrow-right-from-bracket"></i>
+                            <p>Logout</p>
+                        </a>
 
-        <div class="content-wrapper">
-            @yield('content')
+                        <!-- Hidden Logout Form -->
+                        <form id="logout-form" action="{{ route('dokter.logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                @endif
+            @endif
         </div>
 
-        <footer class="main-footer">
-            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-            All rights reserved.
-            <div class="float-right d-none d-sm-inline-block">
-                <b>Version</b> 3.2.0
+
+        <!-- Main Content -->
+        <div class="main-content">
+            <!-- Header -->
+            <div class="header">
+                @yield('header')
             </div>
-        </footer>
+            <!-- Dashboard Cards -->
+            @php
+                $showTables = $showTables ?? true;
+                $showFullscreen = $showFullscreen ?? false;
+            @endphp
 
-        <!-- Control Sidebar -->
-        <aside class="control-sidebar control-sidebar-dark">
-            <!-- Control sidebar content goes here -->
-        </aside>
-        <!-- /.control-sidebar -->
+            <div class="dashboard-cards">
+                @yield('content')
+            </div>
+
+            @if($showTables)
+                <div class="dashboard-tables">
+                    <div class="table-container">
+                        @yield('table')
+                    </div>
+                    <div class="table-container">
+                        @yield('schedule')
+                    </div>
+                </div>
+            @endif
+
+            @if($showFullscreen)
+                <div class="fullscreen-content">
+                    @yield('fullscreen')
+                </div>
+            @endif
+
+            <!-- Dashboard Tables -->
+        </div>
     </div>
-    <!-- ./wrapper -->
 
-    <!-- jQuery -->
-    <script src="{{asset('lte/plugins/jquery/jquery.min.js')}}"></script>
-    <!-- jQuery UI 1.11.4 -->
-    <script src="{{asset('lte/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
-    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
     <script>
-        $.widget.bridge('uibutton', $.ui.button)
+        // Simple navigation functionality
+        document.addEventListener('DOMContentLoaded', function () {
+            const menuItems = document.querySelectorAll('.menu-item');
+
+            menuItems.forEach(item => {
+                item.addEventListener('click', function () {
+                    // Remove active class from all menu items
+                    menuItems.forEach(i => i.classList.remove('active'));
+
+                    // Add active class to clicked item
+                    this.classList.add('active');
+
+                    // Here you would typically handle navigation or view changes
+                    // For demonstration purposes, we're just updating the active state
+                });
+            });
+        });
     </script>
-    <!-- Bootstrap 4 -->
-    <script src="{{asset('lte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-    <!-- ChartJS -->
-    <script src="{{asset('lte/plugins/chart.js/Chart.min.js')}}"></script>
-    <!-- Sparkline -->
-    <script src="{{asset('lte/plugins/sparklines/sparkline.js')}}"></script>
-    <!-- JQVMap -->
-    <script src="{{asset('lte/plugins/jqvmap/jquery.vmap.min.js')}}"></script>
-    <script src="{{asset('lte/plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script>
-    <!-- jQuery Knob Chart -->
-    <script src="{{asset('lte/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
-    <!-- daterangepicker -->
-    <script src="{{asset('lte/plugins/moment/moment.min.js')}}"></script>
-    <script src="{{asset('lte/plugins/daterangepicker/daterangepicker.js')}}"></script>
-    <!-- Tempusdominus Bootstrap 4 -->
-    <script src="{{asset('lte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
-    <!-- Summernote -->
-    <script src="{{asset('lte/plugins/summernote/summernote-bs4.min.js')}}"></script>
-    <!-- overlayScrollbars -->
-    <script src="{{asset('lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
-    <!-- AdminLTE App -->
-    <script src="{{asset('lte/dist/js/adminlte.js')}}"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="{{asset('lte/dist/js/demo.js')}}"></script>
-    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-    <script src="{{asset('lte/dist/js/pages/dashboard.js')}}"></script>
 </body>
 
 </html>
