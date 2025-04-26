bf597569d6f6ac517320d9a68483fb0ef26e8dec
Ariq Farhan <ariqalthaf445@gmail.com>
2025-04-26 17:41:47 +0700

    Refactor : edit form layout and improve input structure

--- resources/views/dokter/edit.blade.php
+++ resources/views/dokter/edit.blade.php
@@ -1,61 +1,63 @@
+@php
+    $showTables = false;
+    $showFullscreen = true;
+@endphp
+
 @extends('layout.app')
 @section('title', 'Obat')
-@section('content')
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0">Obat</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Dashboard v1</li>
-                    </ol>
-                </div><!-- /.col -->
+@section('fullscreen')
+    <div class="fullscreen-form">
+        <div class="form-container">
+            <div class="form-header">
+                <h2><i class="fas fa-pills"></i> Edit Obat</h2>
+                <p class="form-subtitle">Edit informasi detail obat</p>
             </div>
-        </div>
-    </div>
 
-    <section class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <!-- general form elements -->
-                    <div class="card card-primary">
-                        <div class="card-header">
-                            <h3 class="card-title">Form Tambah Obat</h3>
+            <form action="{{ route('dokter.obat.update', $obats->id) }}" method="POST">
+                @csrf
+                @method('PUT')
+                <div class="form-row">
+                    <div class="form-column">
+                        <div class="form-group">
+                            <label for="nama_obat">Nama Obat</label>
+                            <div class="input-wrapper">
+                                <input type="text" id="nama_obat" name="nama_obat" placeholder="Masukkan nama obat"
+                                    value="{{ $obats->nama_obat }}" required>
+                                <i class="fas fa-capsules"></i>
+                            </div>
+                            <div class="input-help">Masukkan nama lengkap obat</div>
                         </div>
-                        <form action="{{ route('dokter.obat.update', $obats->id) }}" method="POST">
-                            @csrf
-                            @method('PUT')
+                    </div>
 
-                            <div class="card-body">
-                                <div class="form-group">
-                                    <label for="nama_obat">Nama Obat</label>
-                                    <input type="text" class="form-control" name="nama_obat" value="{{ $obats->nama_obat }}"
-                                        required>
-
-                                </div>
-                                <div class="form-group">
-                                    <label for="kemasan">Kemasan</label>
-                                    <input type="text" class="form-control" name="kemasan" value="{{ $obats->kemasan }}"
-                                        required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="harga">Harga</label>
-                                    <input type="number" class="form-control" name="harga" value="{{ $obats->harga }}"
-                                        required>
-                                </div>
+                    <div class="form-column">
+                        <div class="form-group">
+                            <label for="kemasan">Kemasan</label>
+                            <div class="input-wrapper">
+                                <input type="text" id="kemasan" name="kemasan" placeholder="Masukkan kemasan obat"
+                                    value="{{ $obats->kemasan }}" required>
+                                <i class="fas fa-box"></i>
                             </div>
-                            <div class="card-footer text-right">
-                                <button type="submit" class="btn btn-primary">Update</button>
-                                <a href="{{ route('dokter.obat') }}" class="btn btn-danger">Batal</a>
-                            </div>
-                        </form>
+                            <div class="input-help">Contoh: Botol 100ml, Strip 10 tablet, dll</div>
+                        </div>
                     </div>
                 </div>
-            </div>
+
+                <div class="form-group">
+                    <label for="harga">Harga (Rp)</label>
+                    <div class="input-wrapper fee">
+                        <input type="number" id="harga" name="harga" placeholder="Masukkan harga obat"
+                            value="{{ $obats->harga }}" required>
+                        <i class="fas fa-tag"></i>
+                    </div>
+                    <div class="input-help">Masukkan harga dalam bentuk angka tanpa tanda titik atau koma</div>
+                </div>
+
+                <div class="form-action">
+                    <button type="submit" class="submit-btn">
+                        <i class="fas fa-save"></i> Update Obat
+                    </button>
+                </div>
+            </form>
         </div>
-    </section>
+    </div>
 @endsection
